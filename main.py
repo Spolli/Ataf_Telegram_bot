a678e743f04300a8ba09b87d8094568e72c7cc0d
QCau9SMPSTh9 <kebabnopiccante@gmail.com>
2020-09-21 13:38:31 +0200

    aggiunte funzioni di ricerca della fermata

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import urllib3
+import requests
 import json
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 
@@ -10,18 +10,46 @@ from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-http = urllib3.PoolManager()
 
 ### USER CONFIG
 bot_token = "INSERT TOKEN HERE"
 ### END USER CONFIG
 
 
-def getAllStops():
-    r = http.request("http://www.temporealeataf.it/Mixer/Rest/PublicTransportService.svc/stops?urLat=44&urLon=12&llLat=43&llLon=10&getId=true")
-    stops = json.loads(r)
+def getSingleStop(stop):
+    r = requests.get(f'http://www.temporealeataf.it/Mixer/Rest/PublicTransportService.svc/single?Lat={stop["y"]}&Lon={stop["x"]}&nodeId={stop["id"]}&getSchedule=true').json()
+    print(r["s"])
 
+def update_stops():
+    stops = requests.get('http://www.temporealeataf.it/Mixer/Rest/PublicTransportService.svc/stops?urLat=44&urLon=12&llLat=43&llLon=10&getId=true').json()
+    with open('src/data/stops.json', 'w', encoding='utf-8') as f:
+        json.dump(stops, f, ensure_ascii=False, indent=4)
+    print("Updated!")
+    
+def search_for_id(id):
+    with open('src/data/stops.json', 'r') as j:
+        stops = json.loads(j.read())
+    for stop in stops:
+        if stop["id"] == id:
+            getSingleStop(stop)
+    print("Nessuna fermata trovata")
 
+def search_for_name(name):
+    with open('src/data/stops.json', 'r') as j:
+        stops = json.loads(j.read())
+    stop_list = []
+    for stop in stops:
+        if name in stop["n"].strip():
+            stop_list.append(stop)
+
+    if len(stop_list) is []:
+        print("Nessun risulato trovato!")
+    elif len(stop_list) == 1:
+        getSingleStop(stop_list[0])
+    else:
+        #caselle per scelta
+        None
+    
 def error_handler(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
@@ -40,8 +68,8 @@ def main():
     updater.start_polling()
     updater.idle()
     '''
-    print(getAllStops())
-
+    #update_stops()
+    search_for_id("FM1153")
 
 if __name__ == '__main__':
     main()
