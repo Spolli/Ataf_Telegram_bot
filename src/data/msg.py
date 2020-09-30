36f13d9155352616c1ac587a3e66fa25f4e944d4
QCau9SMPSTh9 <kebabnopiccante@gmail.com>
2020-09-30 12:44:58 +0200

    remove useless data

---
+++ src/data/msg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+WELCOME_msg = "Benvenuto/a nel nuovo fiammante bot dell'ataf. Amatorialemte fatto da @github.com/Spolli. Buona corsa su quello scatolotto con le ruote!"
+CLOSE_msg = "Grazie per aver usato il bot. Buona fortuna e che i controllori non siano con te (infami)"
+ERROR_msg = "Upsy, un errore è capitato!"
+ENTER_ID_msg = "Inserisci l'ID della fermata. Lo trovi in alto a destra sul cartello. \nEs. AA1234"
+ENTER_NAME_msg = "Inserisci il nome della fermata"
+NO_BUS_msg = "Non passa nessun bus, mi dispy. Ti tocca piottare"
+NO_STOP_msg = "Nessuna fermata trovata, prova a cercare di nuovo"
+
+choosing_keyboard = [['Cerca Per ID fermata'],
+                  ['Cerca per nome della fermata'],
+                  ['Fine']]
